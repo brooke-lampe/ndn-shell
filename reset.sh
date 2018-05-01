d041c219da727758d14141929225214d52b57a40
Brooke E Lampe <blampe@cse.unl.edu>
2018-05-01 17:37:53 -0500

    Reset C1 node

---
+++ reset.sh
@@ -0,0 +1,9 @@
+ndf-stop
+nfd-start
+nfdc face create tcp://10.10.5.1
+nfdc route add /ndn tcp://10.10.5.1
+nfdc face create tcp://10.10.6.1
+nfdc route add /ndn tcp://10.10.6.1
+nfdc face create tcp://10.10.7.1
+nfdc route add /ndn tcp://10.10.7.1
+nfdc strategy set prefix / strategy /localhost/nfd/strategy/load-balancer-strategy
